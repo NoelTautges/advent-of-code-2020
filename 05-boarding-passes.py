5dea37f8db2402e8e7e614bf6ac0d5d8a28e89bf
Noel Tautges <24325426+NoelTautges@users.noreply.github.com>
2020-12-05 23:17:58 -0600

    Complete day 5 part 2

--- 05-boarding-passes.py
+++ 05-boarding-passes.py
@@ -17,15 +17,28 @@ def get_location(binary, upper, upper_char):
 
 def main():
     max_id = 0
+    seats = [[False] * 8 for _ in range(128)]
 
     with open("05.txt") as passes_file:
         for line in passes_file:
-            if (seat_id := get_location(line[:7], 127, "B") * 8 \
-                + get_location(line[7:10], 7, "R")) \
-                > max_id:
+            row = get_location(line[:7], 127, "B")
+            col = get_location(line[7:10], 7, "R")
+            seats[row][col] = True
+
+            if (seat_id := row * 8 + col) > max_id:
                 max_id = seat_id
     
     print(f"Max seat ID: {max_id}")
+
+    for row, row_seats in enumerate(seats):
+        for col in range(6):
+            if row_seats[col] and not row_seats[col + 1] and row_seats[col + 2]:
+                print(f"My seat ID: {row * 8 + col + 1}")
+                break
+        else:
+            continue
+
+        break
 
 if __name__ == "__main__":
     main()
