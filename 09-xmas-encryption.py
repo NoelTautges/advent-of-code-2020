64a41b5b90a88abd39e9a598974b7158ef237ce0
Noel Tautges <24325426+NoelTautges@users.noreply.github.com>
2020-12-09 11:58:14 -0600

    Complete day 9 part 2

--- 09-xmas-encryption.py
+++ 09-xmas-encryption.py
@@ -4,6 +4,9 @@ from collections import deque
 
 def main():
     queue = deque([], 25)
+    invalid = -1
+    sum_queue = deque()
+    cur_sum = 0
     
     with open("09.txt") as xmas_file:
         for line in xmas_file:
@@ -13,7 +16,27 @@ def main():
                 or any(val for val in queue if val * 2 != num and num - val in queue):
                 queue.append(num)
             else:
-                print("Odd number out:", num)
+                invalid = num
+                print("Invalid number:", invalid)
+                break
+    
+        xmas_file.seek(0)
+
+        for line in xmas_file:
+            num = int(line)
+
+            if num > invalid:
+                sum_queue.clear()
+                continue
+            
+            while num + cur_sum > invalid:
+                cur_sum -= sum_queue.popleft()
+            
+            sum_queue.append(num)
+            cur_sum += num
+
+            if cur_sum == invalid:
+                print("Smallest + largest = ", min(sum_queue) + max(sum_queue))
                 break
 
 if __name__ == "__main__":
