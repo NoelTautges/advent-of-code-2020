a28ac3053b8fc83e4da89126db6c2249192eace2
Noel Tautges <24325426+NoelTautges@users.noreply.github.com>
2020-12-10 06:42:25 -0600

    Day 10: merge loops

--- 10-joltage.py
+++ 10-joltage.py
@@ -8,19 +8,19 @@ def main():
     joltages.append(joltages[-1] + 3)
     
     differences = defaultdict(int)
-
-    for i, joltage in enumerate(joltages[1:]):
-        differences[joltage - joltages[i]] += 1
-    
     ways = defaultdict(int, {0: 1})
 
-    for i, joltage in enumerate(joltages[:-1]):
+    for i, joltage in enumerate(joltages):
+        if i > 0:
+            differences[joltage - joltages[i - 1]] += 1
+        
         for dest in joltages[i + 1:i + 4]:
             if dest - joltage > 3:
                 continue
 
             ways[dest] += ways[joltage]
     
+    print("1-jolt * 3-jolt differences =", differences[1] * differences[3])
     print("Ways to arrange adapters:", ways[joltages[-1]])
 
 if __name__ == "__main__":
